fcbb08fff59614142e34db37fd8208a718c64ba1
lx <45314745+lixi5338619@users.noreply.github.com>
2021-01-25 15:53:25 +0800

    Update __init__.py
    add tools

--- lxpy/__init__.py
+++ lxpy/__init__.py
@@ -1,8 +1,12 @@
 #-*- coding:utf-8 -*-
 # @Author  : lx
 
-__all__ = ['DateGo','create_root_node']
+__all__ = ['DateGo','create_root_node','get_ua','re_xpath',
+           'copy_headers_dict','html_format','jsonp_to_json']
+
 
 from .lxdate import DateGo
 from .lxml_check import create_root_node
+from .lxheader import *
+from .lxtools import *
 
